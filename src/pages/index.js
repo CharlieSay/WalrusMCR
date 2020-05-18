a58e561ab2d6891a1b44f1b22fc4e4bc79ccb5ea
Charlie Say <charlie.say@autotrader.co.uk>
2020-05-18 09:59:28 +0100

    made desktop more messy

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,11 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Folder iconLocation={beKind} altText={'Walrus Manchester Magazine'} text={'Be Kind To Your Mind'} externalLink={'https://drive.google.com/drive/folders/1U_Mi5oHln9N3twOy5i8IIfdwvZl_pC2o?usp=sharing'}/>
-      <Folder iconLocation={justGiving} altText={'Just Giving Page for Walrus Manchester'} text={'Just Giving Donation'} externalLink={'https://www.justgiving.com/fundraising/walrusmcr'}/>
+    <div style={{ maxWidth: `900px`, marginBottom: `1.45rem` }}>
+      <Folder iconLocation={beKind} altText={'Walrus Manchester Magazine'} text={'Be Kind To Your Mind'} externalLink={'https://drive.google.com/drive/folders/1U_Mi5oHln9N3twOy5i8IIfdwvZl_pC2o?usp=sharing'} />
+      <div style={{paddingLeft:`182px`, paddingTop: `13px`}}>
+        <Folder iconLocation={justGiving} altText={'Just Giving Page for Walrus Manchester'} text={'Just Giving Donation'} externalLink={'https://www.justgiving.com/fundraising/walrusmcr'} />
+      </div>
     </div>
   </Layout>
 )
